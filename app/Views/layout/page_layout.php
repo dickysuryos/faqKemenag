6f8511f9b9d397f4878aa25fab1f9a34e752c4f5
dicky suryo <dicky.suryo@detik.com>
2024-08-25 02:06:19 +0700

    test for staging

--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Portal HelpDesk Kemenag Yogyakarta</title>
+	<title>Portal HelpDesk Kemenag Yogyakarta Staging</title>
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
