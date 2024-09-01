174ff936b61bbe06b1da7341f29687d18cabfe0c
dicky suryo <dicky.suryo@detik.com>
2024-09-01 22:09:14 +0700

    fix chat layout

--- app/Config/CodeigniterWebsocket.php
+++ app/Config/CodeigniterWebsocket.php
@@ -15,7 +15,7 @@ use CodeIgniter\Config\BaseConfig;
  */
 class CodeigniterWebsocket extends \Takielias\CodeigniterWebsocket\Config\CodeigniterWebsocket
 {
-    public $host = "0.0.0.0";
+    public $host = "100.105.92.122";
     public $port = 8282;
     public $timer = false;
     public $interval = 1;
