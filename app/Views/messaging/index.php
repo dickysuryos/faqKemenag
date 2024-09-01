491f98d9c1cd3d72fb2a91a18a80d90abca2813a
dicky suryo <dicky.suryo@detik.com>
2024-09-01 23:01:37 +0700

    fix connection

--- app/Views/messaging/index.php
+++ app/Views/messaging/index.php
@@ -112,7 +112,7 @@ $session = session();
 
                 <script>
 
-                    var conn = new WebSocket('ws://0.0.0.0:8080');
+                    var conn = new WebSocket('ws://0.0.0.0:8081');
 
                     var client = {
                         user_id: <?= $session->get('id'); ?>,
