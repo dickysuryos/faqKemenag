757370b94ff48b4b6928dfa383f884e9706fac2f
dicky suryo <dicky.suryo@detik.com>
2024-09-01 09:10:16 +0700

    fix port

--- app/Config/CodeigniterWebsocket.php
+++ app/Config/CodeigniterWebsocket.php
@@ -15,8 +15,8 @@ use CodeIgniter\Config\BaseConfig;
  */
 class CodeigniterWebsocket extends \Takielias\CodeigniterWebsocket\Config\CodeigniterWebsocket
 {
-    public $host = "localhost";
-    public $port = 8080;
+    public $host = "0.0.0.0";
+    public $port = 8282;
     public $timer = false;
     public $interval = 1;
     public $auth = false;
